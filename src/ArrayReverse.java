ae20e42992a760dc40b2ae0c6deee614de2bed92
Lodh <sagar.lodh@capgemini.com>
2023-02-27 17:56:37 +0530

    Initial commit one

---
+++ src/ArrayReverse.java
@@ -0,0 +1,10 @@
+import dsa.util.ReverseArray;
+
+public class ArrayReverse {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Java, again!!");
+        Integer[] arr = {10, 20, 30, 40, 50};
+        ReverseArray.reverse(arr);
+    }
+}
